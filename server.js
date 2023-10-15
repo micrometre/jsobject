3b4841c3bd99ba598ff37a90601c82f9e4a0115e
Henok <henokmicrometre@gmail.com>
2023-10-15 11:14:15 +0100

    scafolding

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express")
+
+
+var app = express()
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+
+app.listen(4000)
+console.log("http://localhost:4000")
